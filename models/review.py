d91f3d3875b99e010180188a2c9bfa6e57a4aa93
Karim-308 <105867500+Karim-308@users.noreply.github.com>
2024-03-10 22:28:42 +0200

    Update review.py

--- models/review.py
+++ models/review.py
@@ -1,12 +1,18 @@
 #!/usr/bin/python3
+"""Defines the Review class."""
 from models.base_model import BaseModel
 
-class Review(BaseModel):
-    """Defines the Review class."""
 
-    def __init__(self, *args, **kwargs):
-        """Initialize Review instance."""
-        super().__init__(*args, **kwargs)
-        self.place_id = ""
-        self.user_id = ""
-        self.text = ""
+class Review(BaseModel):
+    """This is a review class definition.
+
+    Attributes:
+        user_id (str): The User id.
+        text (str): The text of the review.        
+        place_id (str): The Place id.
+
+    """
+
+    user_id = ""
+    place_id = ""
+    text = ""
