46d9805e4130d13965839f9f0a144625a0b1bba9
Karim-308 <105867500+Karim-308@users.noreply.github.com>
2024-03-10 22:36:03 +0200

    Update amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
+"""This file define an Amenity clas."""
 from models.base_model import BaseModel
 
-class Amenity(BaseModel):
-    """Defines the Amenity class."""
 
-    def __init__(self, *args, **kwargs):
-        """Initialize Amenity instance."""
-        super().__init__(*args, **kwargs)
-        self.name = ""
+class Amenity(BaseModel):
+    """Represent an amenity.
+
+    Attributes:
+        name (str): The amenity's name.
+    """
+
+    name = ""
