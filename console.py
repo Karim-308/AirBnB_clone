5b6d95122073a7a618f1063d15c6317ab0fffedf
Karim-308 <105867500+Karim-308@users.noreply.github.com>
2024-03-10 20:28:15 +0200

    Update console.py

--- console.py
+++ console.py
@@ -1,31 +1,51 @@
 #!/usr/bin/python3
-"""Module for the HBNBCommand class."""
+"""Module for BaseModel class."""
 
-import cmd
-
-
-class HBNBCommand(cmd.Cmd):
-    """Command interpreter for HBNB project."""
-
-    prompt = '(hbnb) '
-
-    def do_quit(self, arg):
-        """Quit command to exit the program."""
-        return True
-
-    def do_EOF(self, arg):
-        """EOF command to exit the program."""
-        print()
-        return True
-
-    def do_help(self, arg):
-        """Help command to display help messages."""
-        cmd.Cmd.do_help(self, arg)
-
-    def emptyline(self):
-        """Handle empty input lines."""
-        pass
+import uuid
+from datetime import datetime
+from models import storage
 
 
-if __name__ == '__main__':
-    HBNBCommand().cmdloop()
+
+class BaseModel:
+    """Defines the BaseModel class."""
+
+    def __init__(self, *args, **kwargs):
+        """Initialize a new BaseModel instance."""
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != '__class__':
+                    if key == 'created_at' or key == 'updated_at':
+                        setattr(self, key, datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f"))
+                    else:
+                        setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            now = datetime.now()
+            self.created_at = now
+            self.updated_at = now
+
+    def __set_attributes(self, kwargs):
+        """Set instance attributes from dictionary."""
+        date_keys = ['created_at', 'updated_at']
+        for key, value in kwargs.items():
+            if key in date_keys:
+                value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+            setattr(self, key, value)
+
+    def __str__(self):
+        """Return a string representation of the BaseModel instance."""
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+
+    def save(self):
+        """Update the updated_at attribute with the current datetime."""
+        self.updated_at = datetime.now()
+        storage.save()
+
+    def to_dict(self):
+        """Return a dictionary representation of the BaseModel instance."""
+        obj_dict = self.__dict__.copy()
+        obj_dict['__class__'] = self.__class__.__name__
+        obj_dict['created_at'] = self.created_at.isoformat()
+        obj_dict['updated_at'] = self.updated_at.isoformat()
+        return obj_dict
