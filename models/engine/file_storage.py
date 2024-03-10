069660b18204aeda8106c77ac712d86f809ecc1b
Karim-308 <105867500+Karim-308@users.noreply.github.com>
2024-03-10 16:33:38 +0200

    Update file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,15 +1,21 @@
 # models/engine/file_storage.py
 
 import json
+from models.base_model import BaseModel
+from models.user import User
 
 class FileStorage:
+        """Class for managing serialization and deserialization of objects to JSON."""
+    
     __file_path = "file.json"
     __objects = {}
 
     def all(self):
+         """Return the dictionary __objects."""
         return self.__objects
 
     def new(self, obj):
+        """Set in __objects the obj with key <obj class name>.id."""
         key = "{}.{}".format(obj.__class__.__name__, obj.id)
         self.__objects[key] = obj
 
