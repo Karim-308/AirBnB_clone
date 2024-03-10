7a6fe323b4fa1527e55528020aace56bcc4de6a0
Karim-308 <105867500+Karim-308@users.noreply.github.com>
2024-03-10 20:29:53 +0200

    Update file_storage.py
    The last four commit were to get basck from stable

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,38 +1,32 @@
 # models/engine/file_storage.py
 
 import json
-from models.base_model import BaseModel
-from models.user import User
 
 class FileStorage:
-        """Class for managing serialization and deserialization of objects to JSON."""
-    
     __file_path = "file.json"
     __objects = {}
 
     def all(self):
-         """Return the dictionary __objects."""
         return self.__objects
 
     def new(self, obj):
-        """Set in __objects the obj with key <obj class name>.id."""
         key = "{}.{}".format(obj.__class__.__name__, obj.id)
         self.__objects[key] = obj
 
     def save(self):
-        """Serialize __objects to the JSON file __file_path."""
-        obj_dict = {key: obj.to_dict() for key, obj in self.__objects.items()}
-        with open(FileStorage.__file_path, "w") as f:
-            json.dump(obj_dict, f)
+        """Save the objects to the JSON file. , TOBE checked again"""
+        with open(self.__file_path, "w") as file:
+            json.dump({k: v.to_dict() for k, v in self.__objects.items()}, file)
 
+ 
     def reload(self):
-        """Deserialize the JSON file __file_path to __objects, if it exists."""
+        """Reload the objects from the JSON file. TOBE checked again """
         try:
-            with open(FileStorage.__file_path) as f:
-                obj_dict = json.load(f)
-                for key, value in obj_dict.items():
+            with open(self.__file_path, "r") as file:
+                data = json.load(file)
+                for key, value in data.items():
                     class_name, obj_id = key.split('.')
                     cls = eval(class_name)
                     self.__objects[key] = cls(**value)
         except FileNotFoundError:
-            return
+            pass
