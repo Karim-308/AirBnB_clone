4cfc658d951bbdd1021022de3285e61a55f0b1ea
Karim-308 <105867500+Karim-308@users.noreply.github.com>
2024-03-10 14:42:56 +0200

    Create city.py

---
+++ models/city.py
@@ -0,0 +1,16 @@
+# models/city.py
+
+from models.base_model import BaseModel
+
+class City(BaseModel):
+    """Defines the City class."""
+    
+    state_id = ""  # Example attribute specific to City class
+    name = ""      # Example attribute specific to City class
+
+    def __init__(self, *args, **kwargs):
+        """Initialize a new City instance."""
+        super().__init__(*args, **kwargs)
+        if kwargs:
+            self.state_id = kwargs.get('state_id', "")
+            self.name = kwargs.get('name', "")
