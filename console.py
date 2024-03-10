b84978f3d04b145730b73149f9d2bf73dcc09ec1
Karim-308 <105867500+Karim-308@users.noreply.github.com>
2024-03-10 20:35:20 +0200

    Update console.py  till first console task 6

--- console.py
+++ console.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Module for BaseModel class."""
 
+import cmd
 import uuid
 from datetime import datetime
 from models import storage
@@ -49,3 +50,31 @@ class BaseModel:
         obj_dict['created_at'] = self.created_at.isoformat()
         obj_dict['updated_at'] = self.updated_at.isoformat()
         return obj_dict
+
+
+
+class HBNBCommand(cmd.Cmd):
+    """Command interpreter for HBNB project."""
+
+    prompt = '(hbnb) '
+
+    def do_quit(self, arg):
+        """Quit command to exit the program."""
+        return True
+
+    def do_EOF(self, arg):
+        """EOF command to exit the program."""
+        print()
+        return True
+
+    def do_help(self, arg):
+        """Help command to display help messages."""
+        cmd.Cmd.do_help(self, arg)
+
+    def emptyline(self):
+        """Handle empty input lines."""
+        pass
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
