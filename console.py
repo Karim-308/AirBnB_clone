a9eaac087c22828dab91eac370cc67f94bffdeff
Karim-308 <105867500+Karim-308@users.noreply.github.com>
2024-03-10 21:21:52 +0200

    Update console.py testing removeing the basemodel coed form the console

--- console.py
+++ console.py
@@ -8,50 +8,6 @@ from models import storage
 
 
 
-class BaseModel:
-    """Defines the BaseModel class."""
-
-    def __init__(self, *args, **kwargs):
-        """Initialize a new BaseModel instance."""
-        if kwargs:
-            for key, value in kwargs.items():
-                if key != '__class__':
-                    if key == 'created_at' or key == 'updated_at':
-                        setattr(self, key, datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f"))
-                    else:
-                        setattr(self, key, value)
-        else:
-            self.id = str(uuid.uuid4())
-            now = datetime.now()
-            self.created_at = now
-            self.updated_at = now
-
-    def __set_attributes(self, kwargs):
-        """Set instance attributes from dictionary."""
-        date_keys = ['created_at', 'updated_at']
-        for key, value in kwargs.items():
-            if key in date_keys:
-                value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
-            setattr(self, key, value)
-
-    def __str__(self):
-        """Return a string representation of the BaseModel instance."""
-        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
-
-    def save(self):
-        """Update the updated_at attribute with the current datetime."""
-        self.updated_at = datetime.now()
-        storage.save()
-
-    def to_dict(self):
-        """Return a dictionary representation of the BaseModel instance."""
-        obj_dict = self.__dict__.copy()
-        obj_dict['__class__'] = self.__class__.__name__
-        obj_dict['created_at'] = self.created_at.isoformat()
-        obj_dict['updated_at'] = self.updated_at.isoformat()
-        return obj_dict
-
-
 
 class HBNBCommand(cmd.Cmd):
     """Command interpreter for HBNB project."""
