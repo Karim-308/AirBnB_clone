1944f42e2ad2a4c908988b05b10f6d6c4376a674
Karim-308 <105867500+Karim-308@users.noreply.github.com>
2024-03-11 17:12:39 +0200

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -40,7 +40,7 @@ class BaseModel:
     def save(self):
         """Update the updated_at attribute with the current datetime."""
         self.updated_at = datetime.now()
-        storage.save()
+        models.storage.save()
 
     def to_dict(self):
         """Return a dictionary representation of the BaseModel instance."""
