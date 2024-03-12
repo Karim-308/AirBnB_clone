f59784c9dbe5b914f2115b67972c4f90183df78a
Karim-308 <105867500+Karim-308@users.noreply.github.com>
2024-03-12 03:06:30 +0200

    Update test_console.py

--- tests/test_console.py
+++ tests/test_console.py
@@ -273,13 +273,13 @@ class TestConsole(unittest.TestCase):
     def test_show_missing_id_dot_notation(self):
         correct = "** instance id missing **"
         with patch("sys.stdout", new=StringIO()) as output:
+            self.assertFalse(HBNBCommand().onecmd("State.show()"))
+            self.assertEqual(correct, output.getvalue().strip())
+        with patch("sys.stdout", new=StringIO()) as output:
             self.assertFalse(HBNBCommand().onecmd("BaseModel.show()"))
             self.assertEqual(correct, output.getvalue().strip())
         with patch("sys.stdout", new=StringIO()) as output:
-            self.assertFalse(HBNBCommand().onecmd("User.show()"))
-            self.assertEqual(correct, output.getvalue().strip())
-        with patch("sys.stdout", new=StringIO()) as output:
-            self.assertFalse(HBNBCommand().onecmd("State.show()"))
+            self.assertFalse(HBNBCommand().onecmd("Place.show()"))
             self.assertEqual(correct, output.getvalue().strip())
         with patch("sys.stdout", new=StringIO()) as output:
             self.assertFalse(HBNBCommand().onecmd("City.show()"))
@@ -288,7 +288,7 @@ class TestConsole(unittest.TestCase):
             self.assertFalse(HBNBCommand().onecmd("Amenity.show()"))
             self.assertEqual(correct, output.getvalue().strip())
         with patch("sys.stdout", new=StringIO()) as output:
-            self.assertFalse(HBNBCommand().onecmd("Place.show()"))
+            self.assertFalse(HBNBCommand().onecmd("User.show()"))
             self.assertEqual(correct, output.getvalue().strip())
         with patch("sys.stdout", new=StringIO()) as output:
             self.assertFalse(HBNBCommand().onecmd("Review.show()"))
